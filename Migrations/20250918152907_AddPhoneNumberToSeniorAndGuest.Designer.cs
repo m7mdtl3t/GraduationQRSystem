5c279509a374fab29138aca9d17ea2d9be39c21c
mario33-11 <mariokareem656@gmail.com>
2025-09-18 18:32:45 +0300

    Add migration for PhoneNumber columns

--- Migrations/20250918152907_AddPhoneNumberToSeniorAndGuest.Designer.cs
+++ Migrations/20250918152907_AddPhoneNumberToSeniorAndGuest.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GraduationQRSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GraduationQRSystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250918152907_AddPhoneNumberToSeniorAndGuest")]
+    partial class AddPhoneNumberToSeniorAndGuest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
